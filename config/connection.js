a3afbd2e941825a6eb5ee66201b5e5211fa629e8
Aditya Nair <adinair.in@gmail.com>
2022-12-16 17:02:38 -0600

    Added DB class in index.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
-require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = new mysql.createConnection(
     {
         host: "localhost",
-        databse: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+        databse: 'company_db',
+        user: 'user',
+        password:'',
     },
-    console.log(`Connected to the database`)
+    console.log(`Connected to the employee database`)
 );
+
+connection.connect((err) => {
+    if (err) {
+        throw err;
+    };
+});
 
 module.exports = connection;
